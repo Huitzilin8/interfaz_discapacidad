8f84ef5228b4edd4ab641fb922da667955328c25
joses <josesesi1@gmail.com>
2025-10-23 11:31:51 -0600

    Arreglando problemas de docker...

--- modelo.py
+++ modelo.py
@@ -76,12 +76,7 @@ class YoloDockerThread(threading.Thread):
         # Using bash -l to load login shell environment
         bash_cmd = (
             "bash -lc '"
-            "source /ultralytics/venvs/onvif_env/bin/activate && "
-            "yolo predict "
-            "model=far_signals3_elmejor.engine "
-            "source=rtsp://admin:Kalilinux363@192.168.100.72:554/stream "
-            "imgsz=640,640 "
-            "conf=0.30"
+            "ls"
             "'"
         )
         
